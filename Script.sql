b522fdb1c1824f5113ab8acb9f7e8309ef0d3661
AmirrezaTaleb <55345142+AmirrezaTaleb@users.noreply.github.com>
2022-11-05 14:43:13 +0330

    Update Script.sql

--- Script.sql
+++ Script.sql
@@ -2,7 +2,7 @@
 DECLARE @Guid UNIQUEIDENTIFIER ='2F9EC6C6-3127-412E-A3B6-4F78CDC9E689'
 DECLARE @SQLQuerys NVARCHAR(MAX)=(
 
-SELECT TOP 250  'select  count(*)Counts,'''+t.name+'''TableName, '''+c.name+'''ColumnName ,'''+'select * from '+t.name+' where '+c.name+'=''''''+cast('+c.name+' as nvarchar(100))'+'+ '''''''' '+' from '+t.name+' group by '+c.name+' having  '+c.name+'= '''+CAST(@Guid AS NVARCHAR(50))+''' and count( * ) >0 Union All  '
+SELECT    'select  count(*)Counts,'''+t.name+'''TableName, '''+c.name+'''ColumnName ,'''+'select * from '+t.name+' where '+c.name+'=''''''+cast('+c.name+' as nvarchar(100))'+'+ '''''''' '+' from '+t.name+' group by '+c.name+' having  '+c.name+'= '''+CAST(@Guid AS NVARCHAR(50))+''' and count( * ) >0 Union All  '
 FROM sys.columns c
      JOIN sys.tables t ON c.object_id=t.object_id
 WHERE system_type_id=36 AND t.name NOT LIKE '%temp%'
